ff2e0d73c52ecb2ce504d98c28c9c939cc01e329
Doug M. Harris <dharris@hq.xsilo.com>
2019-09-06 10:46:40 -0400

    Create scripts for identity pass-around attributes

---
+++ opt/shibboleth-idp/conf/claimSourceAttributeDefn.js
@@ -0,0 +1,18 @@
+var logger = Java.type("org.slf4j.LoggerFactory").getLogger("net.shibboleth.idp.attribute");
+
+var recipientId = resolutionContext.getAttributeRecipientID();
+logger.info("Attribute Recipient ID {} ", recipientId);
+
+var transientIds = transientId.getValues().iterator();
+
+if (transientIds.hasNext()) { //Assume only one
+   var claimSource = persistentIds.next();
+   var matches = claimSource.match(/^([^\|]+)\|([^\|]+)\|(.*)$/);
+   var expiry = matches[1];
+   var rpEntity = matches[2];
+   
+   if (new Date().getTime() > parseInt(expiry)
+       && rpEntity === recipienmtId) {
+         
+   claimSource.addValue(profile);
+}
